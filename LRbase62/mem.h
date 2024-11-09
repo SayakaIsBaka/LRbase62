658eb16afd4d86a8a7ac2a3670b3b39aa54a24a0
Sayaka <alice@lolita.technology>
2024-11-10 00:44:04 +0100

    removed unimplemented function from mem.h, fix pch setting on Release build

--- LRbase62/mem.h
+++ LRbase62/mem.h
@@ -14,6 +14,5 @@ namespace mem
 	char* TrampHook(char* src, char* dst, unsigned int len);
 	char* ScanBasic(char* pattern, char* mask, char* begin, intptr_t size);
 	char* ScanInternal(char* pattern, char* mask, char* begin, intptr_t size);
-	char* ScanModIn(char* pattern, char* mask, std::string modName);
 	void WriteMemory(LPVOID address, LPVOID value, int byteNum);
 }
